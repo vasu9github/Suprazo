baaa897ac2d10a1f0c7fee7a76896a203baf0f37
Vasu <vasudhiman105@gmail.com>
2025-09-17 13:46:15 +0530

    Fixed contact path

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import Hero from './pages/Hero'
 import About from './pages/About'
 import Services from './pages/Services'
 import Products from './pages/Products'
-import { Contact } from 'lucide-react'
 import Footer from './component/Footer'
-
+import Contact from './pages/Contact'
 const App = () => {
   return (
     <main>
